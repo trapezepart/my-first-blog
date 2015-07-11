a024cb940100698bb75e5f28e51fd38292638196
Abby <abbity@gmail.com>
2015-07-11 15:07:16 +1000

    fixed spelling errors

--- mysite/settings.py
+++ mysite/settings.py
@@ -101,5 +101,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIS_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
